fd45e7aac11c604e764f07d1927ec8efd5c87ba3
SilenNaihin <silen.naihin@gmail.com>
2021-02-28 16:54:14 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import FadeIn from "react-fade-in";
+import sr from "./sr-logo.png";
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <div className='text-5xl bold'>
+      Hello
+    </div>
+  );
+}
+
+export default App;
